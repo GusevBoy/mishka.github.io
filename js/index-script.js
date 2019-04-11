94d8c3bd73ebc69a0485126b32de3bd7d3af11f6
U-GFX\viktor.gusev <viktor.gusev@21IMP-V-GSV.gfx.dom>
2019-04-11 11:26:03 +0300

    all

---
+++ js/index-script.js
@@ -0,0 +1,28 @@
+
+  var button = document.querySelector(".goods-best__button");
+  var basket = document.querySelector(".goods__link_basket");
+  var adding = document.querySelector(".popup-adding");
+  var overlay = document.querySelector(".popup-overlay");
+  var close = document.querySelector(".popup-adding__button");
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  adding.classList.add("popup-adding_show");
+  overlay.classList.add("popup-overlay_show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  adding.classList.remove("popup-adding_show");
+  overlay.classList.remove("popup-overlay_show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (adding.classList.contains("popup-adding_show")) {
+      adding.classList.remove("popup-adding_show");
+      overlay.classList.remove("popup-overlay_show");
+    }
+  }
+});
